923d6a814d333af556a1ed89b3769b9767b3f2d5
Monica Zhazil <monicazhazil@MacBook-Pro.hitronhub.home>
2020-05-18 10:30:40 -0700

    Fixed Video Component with HTML Video Tag.

--- sprint-one/src/components/video.jsx
+++ sprint-one/src/components/video.jsx
@@ -3,23 +3,18 @@ import "./styles/main.css";
 
 function Video(props) {
   return (
-    <>
-      <div className="video">
-        <div className="video__active">
-          <video className="video__playing"></video>
-          <div className="video__icons">
-            <button className="video__icons--play"></button>
-            <button className="video__icons--time">
-              <span className="video__icons--timer">
-                {props.currentVideo.duration}
-              </span>
-            </button>
-            <button className="video__icons--screen"></button>
-            <button className="video__icons--vol"></button>
-          </div>
-        </div>
-      </div>
-    </>
+    <div className="video">
+      <video
+        className="video__playing"
+        width="100%"
+        height="100%"
+        controls
+        poster="../video/video-list-0.jpg"
+      >
+        <source src="../video/brainstation-sample-video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
   );
 }
 
