6cab1449a6181eae37fb31edd65b62512924d5ac
Monica Zhazil <monicazhazil@MacBook-Pro.hitronhub.home>
2020-05-23 00:03:43 -0700

    Fixed rezising on Main Component for Tablet Device based on Sprint I feedback.

--- sprint-two/src/components/Video-description.jsx
+++ sprint-two/src/components/Video-description.jsx
@@ -17,21 +17,23 @@ function Description(props) {
     <>
       <div className="description">
         <h1 className="description__title">{props.currentVideo.title}</h1>
-        <div className="description__details">
-          <p className="description__details--author">
-            {props.currentVideo.channel}
-          </p>
-          <p className="description__details--date">{formattedDate}</p>
-        </div>
-        <div className="description__icons">
-          <img className="description__icons--views" src={views} alt="" />
-          <span className="description__icons--num">
-            {props.currentVideo.views}
-          </span>
-          <img className="description__icons--likes" src={likes} alt="" />
-          <span className="description__likes-num">
-            {props.currentVideo.likes}
-          </span>
+        <div className="description__details-wrapper">
+          <div className="description__details">
+            <p className="description__details--author">
+              {props.currentVideo.channel}
+            </p>
+            <p className="description__details--date">{formattedDate}</p>
+          </div>
+          <div className="description__icons">
+            <img className="description__icons--views" src={views} alt="" />
+            <span className="description__icons--num">
+              {props.currentVideo.views}
+            </span>
+            <img className="description__icons--likes" src={likes} alt="" />
+            <span className="description__likes-num">
+              {props.currentVideo.likes}
+            </span>
+          </div>
         </div>
       </div>
       <div className="description__text-wrapper">
